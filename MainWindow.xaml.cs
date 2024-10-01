433063a4feb9f7429fe83bc2adb220996430bd87
fasterplaymc <agolobokov1@gmail.com>
2024-10-01 15:26:56 +0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using pr3_golobokov_andrei.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pr3_golobokov_andrei
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            FrmMain.Navigate(new Autho());
+        }
+        private void btnBack_Click(object Sender, RoutedEventArgs e)
+        {
+            FrmMain.GoBack();
+        }
+        private void FrmMain_ContentRendered(object sender, EventArgs e)
+        {
+            if (FrmMain.CanGoBack)
+                btnBack.Visibility = Visibility.Visible;
+            else
+                btnBack.Visibility = Visibility.Hidden;
+        }
+    }
+}
